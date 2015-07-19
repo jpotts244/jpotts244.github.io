2909ce1859983c2f35ee7b283a3af7793931528e
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2015-07-18 22:04:01 -0400

    adds restart game function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 console.log("linked");
+
+
+
+
+
+
 //modals to collect players names:
+var namePlayerOne = function namePlayerOne () {
 var playerOneButton = $('#modal-button-playerone');
 var playerOneModal = $('#player-one-modal');
 var submitButton = $('#close-modal');
@@ -7,7 +14,8 @@ var submitButton = $('#close-modal');
 playerOneButton.on('click', function(){
 	playerOneModal.toggle();
 
-});
+	});
+
 submitButton.on('click', function(){
 	playerOneModal.toggle();
 	playerOneButton.toggle();
@@ -16,43 +24,52 @@ submitButton.on('click', function(){
     $('#your-nameone').append(grabName);
     });
 
+}
+
+
+var namePlayerTwo = function namePlayerTwo() {
 var playerTwoButton = $('#modal-button-playertwo');
 var playerTwoModal = $('#player-two-modal');
 var closeButton = $('#close-modal-playertwo');
 
 playerTwoButton.on('click', function(){
 	playerTwoModal.toggle();
-});
+	
+	});
+
 closeButton.on('click', function(){
 	playerTwoModal.toggle();
 	playerTwoButton.toggle();
 	var grabSecondName = $('#player-two-name').val();
 	$('#your-nametwo').append(grabSecondName);
-	startGame();
-	playerMove();
-});
-
-var startGame = function startGame () {
-	alert("Let's play!");
-
+	
+	});
 }
 
+var startGame = function startGame () {
+	namePlayerOne();
+	namePlayerTwo();
+	playerMove();
+
+} 
+
 var playerMove = function playerMove (){ 
-	var count = 0;
+	var count = 1;
 	$('.box').click(function(){
 	
-		if (count > 9) {
+		if (count > 10) {
 			alert('Game Over');
 
 		}   else if ($(this).children('.ximage').is(':visible') || $(this).children('.oimage').is(':visible')) {
 				return true;
 		
-		}	else if (count % 2 === 0) {
-			$(this).children('.oimage').show();
-			count++;
-				
 		}	else if (count % 2 !== 0){
 			$(this).children('.ximage').show();
+			count++;
+
+		
+		}	else if (count % 2 === 0) {
+			$(this).children('.oimage').show();
 			count++;
 				
 		}	
@@ -66,6 +83,7 @@ var winners = [[0,1,2], [3,4,5], [6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
 var boxArray = [];
 var boxDivs = $('.box');
 
+
 for(var i = 0; i < boxDivs.length; i++){
 	console.log(boxDivs[i]);
 	var boardDivs = boxDivs[i];
@@ -73,5 +91,19 @@ for(var i = 0; i < boxDivs.length; i++){
 	boxArray.push(superBoardDivs);
 	console.log(i);
 }
+
+// var restartGame = function restartGame () {
+// 	$('#start').on('click', function(){
+// 		startGame();
+// 	});
+	
+
+// } 
+
+$("#start").on("click", function(){
+	location.reload();
+});
+
+startGame();
 
 
